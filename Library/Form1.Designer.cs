57edd591645bb454b4069e1721e43c51756ff105
Suslov Igor <susloviv@mail.ru>
2023-11-27 23:14:08 +0500

    Добавьте файлы проекта.

---
+++ Library/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace Library
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.selectDatabaseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.createTablesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveChangesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dGVAutors = new System.Windows.Forms.DataGridView();
+            this.dGVBooks = new System.Windows.Forms.DataGridView();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Автор = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.dataSet1 = new System.Data.DataSet();
+            this.dataTable1 = new System.Data.DataTable();
+            this.dataColumn1 = new System.Data.DataColumn();
+            this.dataColumn2 = new System.Data.DataColumn();
+            this.id_autor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.dGV_autor_book = new System.Windows.Forms.DataGridView();
+            this.cB_autor = new System.Windows.Forms.ComboBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dGVAutors)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dGVBooks)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataTable1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dGV_autor_book)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1131, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.selectDatabaseToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // selectDatabaseToolStripMenuItem
+            // 
+            this.selectDatabaseToolStripMenuItem.Name = "selectDatabaseToolStripMenuItem";
+            this.selectDatabaseToolStripMenuItem.Size = new System.Drawing.Size(155, 22);
+            this.selectDatabaseToolStripMenuItem.Text = "Select database";
+            this.selectDatabaseToolStripMenuItem.Click += new System.EventHandler(this.selectDatabaseToolStripMenuItem_Click);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.createTablesToolStripMenuItem,
+            this.saveChangesToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // createTablesToolStripMenuItem
+            // 
+            this.createTablesToolStripMenuItem.Name = "createTablesToolStripMenuItem";
+            this.createTablesToolStripMenuItem.Size = new System.Drawing.Size(145, 22);
+            this.createTablesToolStripMenuItem.Text = "Create tables";
+            this.createTablesToolStripMenuItem.Click += new System.EventHandler(this.createTablesToolStripMenuItem_Click);
+            // 
+            // saveChangesToolStripMenuItem
+            // 
+            this.saveChangesToolStripMenuItem.Name = "saveChangesToolStripMenuItem";
+            this.saveChangesToolStripMenuItem.Size = new System.Drawing.Size(145, 22);
+            this.saveChangesToolStripMenuItem.Text = "Save changes";
+            this.saveChangesToolStripMenuItem.Click += new System.EventHandler(this.saveChangesToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.CheckFileExists = false;
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // dGVAutors
+            // 
+            this.dGVAutors.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dGVAutors.Location = new System.Drawing.Point(440, 346);
+            this.dGVAutors.Name = "dGVAutors";
+            this.dGVAutors.Size = new System.Drawing.Size(679, 215);
+            this.dGVAutors.TabIndex = 1;
+            this.dGVAutors.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dGVAutors_CellDoubleClick);
+            // 
+            // dGVBooks
+            // 
+            this.dGVBooks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dGVBooks.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.Автор,
+            this.id_autor});
+            this.dGVBooks.Location = new System.Drawing.Point(440, 45);
+            this.dGVBooks.Name = "dGVBooks";
+            this.dGVBooks.Size = new System.Drawing.Size(679, 295);
+            this.dGVBooks.TabIndex = 1;
+            // 
+            // id
+            // 
+            this.id.DataPropertyName = "id";
+            this.id.HeaderText = "№ п/п";
+            this.id.Name = "id";
+            this.id.ReadOnly = true;
+            this.id.Width = 50;
+            // 
+            // Автор
+            // 
+            this.Автор.DataPropertyName = "id_autors";
+            this.Автор.DataSource = this.dataSet1;
+            this.Автор.DisplayMember = "Table1.name";
+            this.Автор.HeaderText = "Автор";
+            this.Автор.Name = "Автор";
+            this.Автор.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Автор.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.Автор.ValueMember = "Table1.id";
+            this.Автор.Width = 200;
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "NewDataSet";
+            this.dataSet1.Tables.AddRange(new System.Data.DataTable[] {
+            this.dataTable1});
+            // 
+            // dataTable1
+            // 
+            this.dataTable1.Columns.AddRange(new System.Data.DataColumn[] {
+            this.dataColumn1,
+            this.dataColumn2});
+            this.dataTable1.TableName = "Table1";
+            // 
+            // dataColumn1
+            // 
+            this.dataColumn1.ColumnName = "id";
+            this.dataColumn1.DataType = typeof(int);
+            // 
+            // dataColumn2
+            // 
+            this.dataColumn2.ColumnName = "name";
+            // 
+            // id_autor
+            // 
+            this.id_autor.DataPropertyName = "id_autors";
+            this.id_autor.HeaderText = "id_autors";
+            this.id_autor.Name = "id_autor";
+            this.id_autor.Width = 70;
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // dGV_autor_book
+            // 
+            this.dGV_autor_book.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dGV_autor_book.Location = new System.Drawing.Point(24, 92);
+            this.dGV_autor_book.Name = "dGV_autor_book";
+            this.dGV_autor_book.Size = new System.Drawing.Size(290, 150);
+            this.dGV_autor_book.TabIndex = 2;
+            // 
+            // cB_autor
+            // 
+            this.cB_autor.FormattingEnabled = true;
+            this.cB_autor.Location = new System.Drawing.Point(24, 45);
+            this.cB_autor.Name = "cB_autor";
+            this.cB_autor.Size = new System.Drawing.Size(290, 21);
+            this.cB_autor.TabIndex = 3;
+            this.cB_autor.SelectedValueChanged += new System.EventHandler(this.cB_autor_SelectedValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1131, 572);
+            this.Controls.Add(this.cB_autor);
+            this.Controls.Add(this.dGV_autor_book);
+            this.Controls.Add(this.dGVBooks);
+            this.Controls.Add(this.dGVAutors);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dGVAutors)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dGVBooks)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataTable1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dGV_autor_book)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem selectDatabaseToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem createTablesToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dGVAutors;
+        private System.Windows.Forms.DataGridView dGVBooks;
+        private System.Windows.Forms.ToolStripMenuItem saveChangesToolStripMenuItem;
+        private System.Data.DataSet dataSet1;
+        private System.Data.DataTable dataTable1;
+        private System.Data.DataColumn dataColumn1;
+        private System.Data.DataColumn dataColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewComboBoxColumn Автор;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id_autor;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.DataGridView dGV_autor_book;
+        private System.Windows.Forms.ComboBox cB_autor;
+    }
+}
+
---
+++ Library/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.Data.SqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Xml.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Net.NetworkInformation;
+
+namespace Library
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private SQLiteConnection conn;        
+        private DataTable dtAutors;
+        private DataTable dtBooks;
+        private DataTable dtAutor_book;
+
+        //private DataSet dtAutors;
+        //private DataSet dtBooks;
+
+        private SQLiteDataAdapter adAutors;
+        private SQLiteDataAdapter adBooks;
+        private SQLiteDataAdapter adAutor_book;
+
+        private void selectDatabaseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string filename;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                filename = openFileDialog1.FileName;
+                conn = new SQLiteConnection("DataSource=" + filename);
+                conn.Open(); // создаст файл 
+                string sqltext = "select name from sqlite_master where type='table';";
+
+                using (SQLiteCommand cmd = new SQLiteCommand(sqltext, conn))
+                {
+                    SQLiteDataReader reader = cmd.ExecuteReader();
+                    createTablesToolStripMenuItem.Enabled = !reader.HasRows; // отключаем кнопку создания таблиц
+                    if (reader.HasRows)
+                    {
+                        dataGridViewFillAutors();
+                        dataGridViewFillBooks();
+                        cB_autorFillAutors();
+
+
+                    }
+                    reader.Close();
+                    conn.Close();
+                }                    
+                //dataGridViewFillAutors();
+                //dataGridViewFillBooks();               
+            }
+            else
+            {
+                //указать что будет если база не откроится Library.db
+            }
+        }
+
+        private void createTablesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string sqltext = "create table Autors(id int PRIMARY KEY, name varchar(20), description varchar(100));" +
+                "create table Books(id int PRIMARY KEY, id_autors int, name varchar(20), description varchar(100));";
+            using (SQLiteCommand cmd = new SQLiteCommand(sqltext, conn))
+            {
+                //cmd.Connection = conn; // эту строку можно убрать conn на строке выше
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+            createTablesToolStripMenuItem.Enabled = false; // отключаем кнопку создания таблиц
+        }
+
+        //private void dataGridViewFill()
+        //{
+        //    string sqltext = "select * from Autors;";
+        //    adAutors = new SQLiteDataAdapter(sqltext, conn);
+        //    SQLiteCommandBuilder cbAutors = new SQLiteCommandBuilder(adAutors);
+        //    dtAutors = new DataTable();
+        //    //dtAutors = new DataSet();
+        //    adAutors.Fill(dtAutors);
+        //    dGVAutors.DataSource = dtAutors;
+
+        //    sqltext = "select * from Books;";
+        //    adBooks = new SQLiteDataAdapter(sqltext, conn);
+        //    SQLiteCommandBuilder cbBooks = new SQLiteCommandBuilder(adBooks);
+        //    dtBooks = new DataTable();
+        //    //dtBooks = new DataSet();
+        //    adBooks.Fill(dtBooks);
+        //    dGVBooks.DataSource = dtBooks;
+        //    // dGVBooks..DataSource = dtBooks.Tables[0];
+
+        //}
+
+        private void dataGridViewFillAutors()
+        {
+            string sqltext = "select * from Autors;";
+            adAutors = new SQLiteDataAdapter(sqltext, conn);
+
+            SQLiteCommandBuilder cbAutors = new SQLiteCommandBuilder(adAutors);
+            dtAutors = new DataTable();            
+            adAutors.Fill(dtAutors);
+            dGVAutors.DataSource = dtAutors;
+            dtAutors.Columns[0].AutoIncrement = true;
+            dtAutors.Columns[0].AutoIncrementSeed = 1;
+            dtAutors.Columns[0].AutoIncrementSeed = IncrementSeed("Autors");
+        }
+        private void dataGridViewFillBooks(string autor_id="")
+        {
+            //string sqltext = "select * from Books" +
+            //    " join Autors on Autors.id = Books.id_autors";
+
+            string sqltext = "select Books.id, Books.id_autors, Autors.name as autor, Books.name as book, Books.description as book_description from Books join Autors on Autors.id = Books.id_autors";
+            if (!string.IsNullOrEmpty(autor_id))
+            {
+                sqltext = sqltext + " where id_autors=" + autor_id;
+            }                
+            dtBooks = new DataTable();
+            adBooks = new SQLiteDataAdapter(sqltext, conn);
+
+            //Вариант 1
+            //SQLiteCommandBuilder cbBooks = new SQLiteCommandBuilder(adBooks);
+            //adBooks.Fill(dtBooks);
+            //dGVBooks.DataSource = dtBooks;
+            //dtBooks.Columns[0].AutoIncrement = true;
+            //dtBooks.Columns[0].AutoIncrementSeed = 1;
+            //dtBooks.Columns[0].AutoIncrementSeed = 10;
+
+            
+            //Вариант 2
+            adBooks.SelectCommand = new SQLiteCommand(sqltext, conn);
+            sqltext = @"insert into Books values (@id, @id_autors, @name, @description);";
+            adBooks.InsertCommand = new SQLiteCommand(sqltext, conn);
+            adBooks.InsertCommand.Parameters.Add("id", DbType.Int32,5, "id");
+            adBooks.InsertCommand.Parameters.Add("id_autors", DbType.Int32, 5, "id_autors");
+            adBooks.InsertCommand.Parameters.Add("name", DbType.String, 20, "name");
+            adBooks.InsertCommand.Parameters.Add("description", DbType.String, 100, "description");
+
+            sqltext = @"update Books set id_autors=@id_autors, name=@name, description=@description;";
+            adBooks.UpdateCommand = new SQLiteCommand(sqltext, conn);
+            adBooks.UpdateCommand.Parameters.Add("id", DbType.Int32, 5, "id");
+            adBooks.UpdateCommand.Parameters.Add("id_autors", DbType.Int32, 5, "id_autors");
+            adBooks.UpdateCommand.Parameters.Add("name", DbType.String, 20, "name");
+            adBooks.UpdateCommand.Parameters.Add("description", DbType.String, 100, "description");
+
+            sqltext = @"delete from Books where id=@id;";
+            adBooks.DeleteCommand = new SQLiteCommand(sqltext, conn);
+            adBooks.DeleteCommand.Parameters.Add("id", DbType.Int32, 5, "id");  
+
+            adBooks.Fill(dtBooks);
+            dGVBooks.DataSource = dtBooks;
+            dtBooks.Columns[0].AutoIncrement = true;
+            dtBooks.Columns[0].AutoIncrementSeed = 1;
+            dtBooks.Columns[0].AutoIncrementSeed = IncrementSeed("Books");
+
+            (dGVBooks.Columns[1] as DataGridViewComboBoxColumn).DataSource = dtAutors;
+            (dGVBooks.Columns[1] as DataGridViewComboBoxColumn).DataPropertyName = "id_autors";
+            (dGVBooks.Columns[1] as DataGridViewComboBoxColumn).ValueMember = "id";
+            (dGVBooks.Columns[1] as DataGridViewComboBoxColumn).DisplayMember = "Name";
+
+
+
+            //adAutors.Fill(dataSet1);
+            //dGVBooks.Columns["Autors.name"].DataSource = dtAutors;
+
+
+        }
+
+        private int IncrementSeed (string tablname)
+        {
+            int seed = 0;
+            string sqltext = "Select max(id) from " + tablname;
+            SQLiteCommand cmd = new SQLiteCommand(sqltext, conn);
+           // conn.Open();
+            SQLiteDataReader dr = cmd.ExecuteReader();
+            if (dr.HasRows)
+            {
+                dr.Read();
+                seed = Convert.ToInt32(dr[0]);
+                //  conn.Close();
+            }
+            return ++seed;
+        }
+        private void saveChangesToolStripMenuItem_Click(object sender, EventArgs e)
+        {            
+            adAutors.Update(dtAutors);            
+            adBooks.Update(dtBooks);
+        }
+
+        private void dGVAutors_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //MessageBox.Show(dGVAutors[0, dGVAutors.CurrentRow.Index].Value.ToString());
+            dataGridViewFillBooks(dGVAutors[0, dGVAutors.CurrentRow.Index].Value.ToString());
+
+
+        }
+        private void cB_autorFillAutors()
+        {
+            string sqltext = "select * from Autors;";
+            SQLiteCommand cmd = new SQLiteCommand(sqltext, conn);
+            SQLiteDataReader dr = cmd.ExecuteReader();
+            
+            while (dr.Read())
+            {
+                cB_autor.Items.Add(dr["name"].ToString());
+            }        
+        }
+
+        private void cB_autor_SelectedValueChanged(object sender, EventArgs e)
+        {
+            string sqltext = "select Books.name as book from Books join Autors on Autors.id = Books.id_autors";
+            sqltext = sqltext + " where Autors.name='" + cB_autor.Text+"'";
+            adAutor_book = new SQLiteDataAdapter(sqltext, conn);
+
+            SQLiteCommandBuilder cbAutor_book = new SQLiteCommandBuilder(adAutor_book);
+            dtAutor_book = new DataTable();
+            adAutor_book.Fill(dtAutor_book);
+            dGV_autor_book.DataSource = dtAutor_book;
+        }
+    }
+}
